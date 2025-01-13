3f91919e657aba70a0419af02204c288dab61ecb
Royweru <royezi17@gmail.com>
2025-01-13 08:40:29 +0300

    modified:   .gitignore 	modified:   api/index.js 	modified:   package-lock.json 	modified:   package.json

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+const uri = process.env.MONGO_URI
+
+console.log(uri)
+
+mongoose.connect(uri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Connection error:', err));
 const app = express()
 
 app.listen(3000, ()=>{
